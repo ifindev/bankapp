e4e42a90cf4655b28f29986dd46f4d51a155e497
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-06-25 17:57:08 +0700

    transfer-tx: added update balance & failed on concurrent update

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -96,12 +96,36 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			AccountID: arg.ToAccountID,
 			Amount:    arg.Amount,
 		})
-
 		if err != nil {
 			return err
 		}
 
-		// TODO: update account's balance --> Need to be careful with db locking & potential deadlock
+		// update account's balance --> Need to be careful with db locking & potential deadlock
+		account1, err := q.GetAccount(ctx, arg.FromAccountID)
+		if err != nil {
+			return err
+		}
+
+		result.FromAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+			ID:      arg.FromAccountID,
+			Balance: account1.Balance - arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		account2, err := q.GetAccount(ctx, arg.ToAccountID)
+		if err != nil {
+			return err
+		}
+
+		result.ToAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+			ID:      arg.ToAccountID,
+			Balance: account2.Balance + arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 
 		return nil
 	})
